02cafffd2a96f9da1d62d88b85dbcaea7c882dd3
Karthik <32044378+Karthik99999@users.noreply.github.com>
2021-04-28 21:13:44 -0400

    replay file stuff

--- js/replay-embed.template.js
+++ js/replay-embed.template.js
@@ -12,28 +12,28 @@ function requireScript(url) {
 	document.head.appendChild(scriptEl);
 }
 
-linkStyle('https://play.pokemonshowdown.com/style/font-awesome.css?');
-linkStyle('https://play.pokemonshowdown.com/style/battle.css?a7');
-linkStyle('https://play.pokemonshowdown.com/style/replay.css?a7');
-linkStyle('https://play.pokemonshowdown.com/style/utilichart.css?a7');
+linkStyle('http://radicalredshowdown.us.to/style/font-awesome.css?');
+linkStyle('http://radicalredshowdown.us.to/style/battle.css?a7');
+linkStyle('http://radicalredshowdown.us.to/style/replay.css?a7');
+linkStyle('http://radicalredshowdown.us.to/style/utilichart.css?a7');
 
-requireScript('https://play.pokemonshowdown.com/js/lib/ps-polyfill.js');
-requireScript('https://play.pokemonshowdown.com/config/config.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/lib/jquery-1.11.0.min.js');
-requireScript('https://play.pokemonshowdown.com/js/lib/lodash.compat.js');
-requireScript('https://play.pokemonshowdown.com/js/lib/html-sanitizer-minified.js');
-requireScript('https://play.pokemonshowdown.com/js/battle-sound.js');
-requireScript('https://play.pokemonshowdown.com/js/battledata.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex-mini.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex-mini-bw.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/graphics.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/moves.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/abilities.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
+requireScript('http://radicalredshowdown.us.to/js/lib/ps-polyfill.js');
+requireScript('http://radicalredshowdown.us.to/config/config.js?a7');
+requireScript('http://radicalredshowdown.us.to/js/lib/jquery-1.11.0.min.js');
+requireScript('http://radicalredshowdown.us.to/js/lib/lodash.compat.js');
+requireScript('http://radicalredshowdown.us.to/js/lib/html-sanitizer-minified.js');
+requireScript('http://radicalredshowdown.us.to/js/battle-sound.js');
+requireScript('http://radicalredshowdown.us.to/js/battledata.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/pokedex-mini.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/pokedex-mini-bw.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/graphics.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/pokedex.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/moves.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/abilities.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/items.js?a7');
+requireScript('http://radicalredshowdown.us.to/data/teambuilder-tables.js?a7');
+requireScript('http://radicalredshowdown.us.to/js/battle-tooltips.js?a7');
+requireScript('http://radicalredshowdown.us.to/js/battle.js?a7');
 
 var Replays = {
 	battle: null,
